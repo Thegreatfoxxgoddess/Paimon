9f2974f686fffc6caf3ec8eaca0430b6fb420bf5
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2021-12-09 22:24:42 +0530

    Update translate.py

--- paimon/plugins/utils/translate.py
+++ paimon/plugins/utils/translate.py
@@ -2,12 +2,13 @@
 #
 # Edited by Alicia
 
+
 import time
 from json import dumps
 
 from googletrans import LANGUAGES, Translator
 
-from paimon import Config, Message, paimon, pool
+from paimon import Config, Message, pool, paimon
 from paimon.utils.functions import get_emoji_regex
 
 
@@ -15,7 +16,10 @@ from paimon.utils.functions import get_emoji_regex
     "tr",
     about={
         "header": "Translate the given text using Google Translate",
-        "supported languages": dumps(LANGUAGES, indent=4, sort_keys=True),
+        "supported languages": dumps(LANGUAGES, indent=4),
+        "flags": {
+            "-s": "secret translation",
+        },
         "usage": "from english to sinhala\n"
         "{tr}tr -en -si i am paimon\n\n"
         "from auto detected language to sinhala\n"
@@ -34,31 +38,51 @@ from paimon.utils.functions import get_emoji_regex
 async def translateme(message: Message):
     text = message.filtered_input_str
     flags = message.flags
-    if message.reply_to_message:
-        text = message.reply_to_message.text or message.reply_to_message.caption
+    if not text:
+        reply_ = message.reply_to_message
+        text = reply_.text or reply_.caption
     if not text:
         await message.err(
             text="Give a text or reply to a message to translate!\nuse `.help tr`"
         )
         return
-    if len(flags) == 2:
-        src, dest = list(flags)
+    if len(flags) == 3:
+        if "s" in flags:
+            src, dest = list(flags)[1], list(flags)[2]
+        else:
+            await message.edit("Language flags can't be more than 2...", del_in=5)
+            return
+    elif len(flags) == 2:
+        if "s" not in flags:
+            src, dest = list(flags)
+        else:
+            src, dest = "auto", list(flags)[1]
     elif len(flags) == 1:
-        src, dest = "auto", list(flags)[0]
+        if "s" not in flags:
+            src, dest = "auto", list(flags)[0]
+        else:
+            src, dest = "auto", Config.LANG
     else:
         src, dest = "auto", Config.LANG
     text = get_emoji_regex().sub("", text)
-    await message.edit("`Translating ...`")
+    if "s" not in flags:
+        await message.edit("`Translating ...`")
     try:
         reply_text = await _translate_this(text, dest, src)
     except ValueError:
-        await message.err(text="Invalid destination language.\nuse `.help tr`")
+        await message.err(
+            text="Invalid destination language.\nuse `.help tr`"
+        ) if "s" not in flags else await message.delete()
         return
-    source_lan = LANGUAGES[f"{reply_text.src.lower()}"]
-    transl_lan = LANGUAGES[f"{reply_text.dest.lower()}"]
-    output = f"**Source ({source_lan.title()}):**`\n{text}`\n\n\
-**Translation ({transl_lan.title()}):**\n`{reply_text.text}`"
-    await message.edit_or_send_as_file(text=output, caption="translated")
+    if "s" not in flags:
+        source_lan = LANGUAGES[f"{reply_text.src.lower()}"]
+        transl_lan = LANGUAGES[f"{reply_text.dest.lower()}"]
+        output = f"Translated {source_lan.title()} to {transl_lan.title()}\n`{reply_text.text}`"
+        caption = "translated"
+    else:
+        output = reply_text.text
+        caption = None
+    await message.edit_or_send_as_file(text=output, caption=caption)
 
 
 @pool.run_in_thread
