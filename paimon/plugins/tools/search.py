b777209fd1022a590de7640d66bd6902899ddf2c
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2021-12-09 22:23:23 +0530

    Update search.py

--- paimon/plugins/tools/search.py
+++ paimon/plugins/tools/search.py
@@ -7,18 +7,18 @@ from paimon import Message, paimon
 
 @paimon.on_cmd(
     "s",
-    about={"header": "procure comandos no paimon", "examples": "{tr}s wel"},
+    about={"header": "search commands in paimon", "examples": "{tr}s wel"},
     allow_channels=False,
 )
 async def search(message: Message):
     cmd = message.input_str
     if not cmd:
-        await message.err(text="Insira uma palavra para procurar um comando")
+        await message.err(text="Enter any keyword to search in commands")
         return
     found = [i for i in sorted(list(paimon.manager.enabled_commands)) if cmd in i]
-    out_str = "    ".join(found)
+    out_str = "`\n`".join(found)
     if found:
-        out = f"**--Eu encontrei ({len(found)}) comandos para-- : `{cmd}`**\n\n`{out_str}`"
+        out = f"**--I found [{len(found)}] commands for-- : `{cmd}`**\n\n`{out_str}`"
     else:
-        out = f"__nenhum comando encontrado para__ : `{cmd}`"
+        out = f"__command not found for__ : `{cmd}`"
     await message.edit(text=out, del_in=0)
