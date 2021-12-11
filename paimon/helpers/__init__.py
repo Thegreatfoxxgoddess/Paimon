4cd4ffa68309d0734a23bc6a4da7b721dd4152ba
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2021-12-11 21:58:50 +0530

    Create __init__.py

---
+++ paimon/helpers/__init__.py
@@ -0,0 +1,13 @@
+# for jutsu_tools
+
+from .jutsu_tools import (
+    capitaled,
+    report_user,
+    time_date_diff,
+    admin_or_creator,
+    admin_chats,
+    get_response,
+    full_name,
+    msg_type,
+    extract_id
+)
