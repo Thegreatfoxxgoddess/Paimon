215009559ad08e70680d780c02fdf46bb168f52d
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2021-12-17 17:19:09 -0300

    Update flac.py

--- paimon/plugins/kawaii/flac.py
+++ paimon/plugins/kawaii/flac.py
@@ -2,112 +2,41 @@
 # before porting please ask to Kakashi
 
 
-from paimon import Config, Message, paimon
-from paimon.helpers import capitaled
+from asyncio import gather
+
+from paimon import paimon, Message
+from paimon.config import Config
 
 
 @paimon.on_cmd(
     "flac",
     about={
-        "header": "download music with FlacBot",
-        "description": "change quality with flac_q",
-        "usage": "{tr}flac [artist and song name]",
+        "header": "FLAC music downloader",
+        "usage": "{tr}flac [song name]",
     },
 )
-async def flac_bot(message: Message):
-    """download music with FlacBot"""
+async def jio_music(message: Message):
+    bot_ = "JioDLBot"
     query_ = message.input_str
-    if not query_:
-        return await message.edit("`Provide input to search...`", del_in=5)
-    query_ = capitaled(query_)
-    await message.edit(f"Searching <b>{query_}</b> on deezer...")
-    bot_ = "FlacStoreBot"
-    results = await paimon.get_inline_bot_results(bot_, query_)
-    if not results:
-        return await message.edit("`Results not found...`", del_in=5)
-    log_send = await paimon.send_inline_bot_result(
-        chat_id=Config.LOG_CHANNEL_ID,
-        query_id=results.query_id,
-        result_id=results.results[0].id,
-    )
-    reply_ = message.reply_to_message
-    if reply_:
-        reply_to = reply_.message_id
-    else:
-        reply_to = None
-    try:
-        await paimon.copy_message(
-            chat_id=bot_,
-            from_chat_id=Config.LOG_CHANNEL_ID,
-            message_id=log_send.updates[0].id,
-        ),
-        async with paimon.conversation(bot_) as conv:
-            await conv.get_response(mark_read=True)
-            music_ = await conv.get_response(mark_read=True)
-        await paimon.copy_message(
-            message.chat.id, bot_, music_.message_id, reply_to_message_id=reply_to
-        )
-        await message.delete()
-    except BaseException as e:
-        await message.edit(
-            f"`Something unexpected happened.`\n<b>Error:</b> `{e}`", del_in=5
-        )
-
-
-@paimon.on_cmd(
-    "flac_q",
-    about={
-        "header": "set quality of flacbot",
-        "flags": {
-            "-c": "check set quality",
-        },
-        "options": {
-            "flac": "FLAC quality",
-            "320": "MP3_320",
-            "256": "MP3_256",
-            "128": "MP3_128",
-        },
-        "usage": "{tr}flac_q flac or 320 or 256 or 128",
-    },
-)
-async def flac_quality(message: Message):
-    """set quality of flacbot"""
-    bot_ = "FlacStoreBot"
-    if "-c" in message.flags:
-        async with paimon.conversation(bot_) as conv:
-            await conv.send_message("/settings")
-            check = await conv.get_response(mark_read=True)
-        quality_ = check.reply_markup.inline_keyboard[0][0].text
-        qual = quality_.split()[1]
-        await message.edit(f"`Current quality set in the bot is: {qual}`", del_in=5)
+    result = await paimon.get_inline_bot_results(bot_, query_)
+    if not result.results:
+        await message.edit(f"Song <code>{query_}</code> not found...", del_in=5)
         return
-    input_ = message.input_str
-    if not input_:
-        return await message.edit("`Provide quality to set...`", del_in=5)
-    if input_ not in ["flac", "320", "256", "128"]:
-        return await message.edit(
-            "`Input not found in available options, see 'help flac_q'...`", del_in=5
+    try:
+        log_ = await paimon.send_inline_bot_result(
+            Config.LOG_CHANNEL_ID,
+            query_id=result.query_id,
+            result_id=result.results[0].id
         )
-    if input_ == "flac":
-        q_ = 0
-        quality = "FLAC"
-    elif input_ == "320":
-        q_ = 1
-        quality = "MP3_320"
-    elif input_ == "256":
-        q_ = 2
-        quality = "MP3_256"
-    else:
-        q_ = 3
-        quality = "MP3_128"
-    await message.edit("`Changing quality...`")
-    async with paimon.conversation(bot_) as conv:
-        try:
-            await conv.send_message("/settings")
-        except BaseException:
-            return await message.edit("`Unblock` @FlacStoreBot `first.`", del_in=5)
-        resp_one = await conv.get_response(mark_read=True)
-        await resp_one.click()
-        resp_two = await conv.get_response(mark_read=True)
-        await resp_two.click(x=q_)
-    await message.edit(f"Changed music quality to <b>{quality}</b>.", del_in=5)
+        await gather(
+            paimon.copy_message(
+                chat_id=message.chat.id,
+                from_chat_id=Config.LOG_CHANNEL_ID,
+                message_id=log_.updates[0].id,
+            ),
+            message.delete(),
+        )
+    except BaseException as e:
+        await message.err(
+            f"`Something unexpected happend.`\n<b>ERROR:</b> `{e}`", del_in=5
+        )
