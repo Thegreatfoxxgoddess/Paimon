d1c050c39bae7a69a6c60231004c4318618cc559
thegreatfoxxgoddess <thegreatfoxxgoddess@yandex.com>
2021-12-19 17:31:57 +0000

    code check

--- paimon/plugins/xtra/imdb.py
+++ paimon/plugins/xtra/imdb.py
@@ -3,30 +3,32 @@
 
 import json
 import os
+from operator import truediv
+
 import requests
 import wget
-from operator import truediv
 from PIL import Image
 
-from paimon import paimon, Message, Config, pool
+from paimon import Config, Message, paimon, pool
 
 THUMB_PATH = Config.DOWN_PATH + "imdb_thumb.jpg"
 API_ONE_URL = os.environ.get("IMDB_API_ONE_URL")
 API_TWO_URL = os.environ.get("IMDB_API_TWO_URL")
 
 
-@paimon.on_cmd("imdb", about={
-    'header': "Scrap Movies & Tv Shows from IMDB",
-    'description': "Get info about a Movie on IMDB.\n"
-                   "[NOTE: To use a custom poster, download "
-                   "the poster with name imdb_thumb.jpg]",
-    'usage': "{tr}imdb [Movie Name]"})
+@paimon.on_cmd(
+    "imdb",
+    about={
+        "header": "Scrap Movies & Tv Shows from IMDB",
+        "description": "Get info about a Movie on IMDB.\n"
+        "[NOTE: To use a custom poster, download "
+        "the poster with name imdb_thumb.jpg]",
+        "usage": "{tr}imdb [Movie Name]",
+    },
+)
 async def imdb(message: Message):
     if not (API_ONE_URL or API_TWO_URL):
-        return await message.err(
-            "`API Error....!`",
-            disable_web_page_preview=True
-        )
+        return await message.err("`API Error....!`", disable_web_page_preview=True)
     try:
         movie_name = message.input_str
         await message.edit(f"__searching IMDB for__ : `{movie_name}`")
@@ -41,7 +43,7 @@ async def imdb(message: Message):
         image_link = first_movie.get("i").get("imageUrl")
         mov_details = get_movie_details(second_page_response)
         director, writer, stars = get_credits_text(second_page_response)
-        story_line = second_page_response.get("summary").get("plot", 'Not available')
+        story_line = second_page_response.get("summary").get("plot", "Not available")
         mov_country, mov_language = get_countries_and_languages(second_page_response)
         mov_rating = second_page_response.get("UserRating").get("description")
         des_ = f"""<b>Title: </b><code>{mov_title}</code>
@@ -66,24 +68,18 @@ async def imdb(message: Message):
     if os.path.exists(THUMB_PATH):
         optimize_image(THUMB_PATH)
         await message.client.send_photo(
-            chat_id=message.chat.id,
-            photo=THUMB_PATH,
-            caption=des_,
-            parse_mode="html"
+            chat_id=message.chat.id, photo=THUMB_PATH, caption=des_, parse_mode="html"
         )
         await message.delete()
     elif image_link is not None:
         await message.edit("__downloading thumb ...__")
         image = image_link
-        img_path = await pool.run_in_thread(
-            wget.download
-        )(image, os.path.join(Config.DOWN_PATH, 'imdb_thumb.jpg'))
+        img_path = await pool.run_in_thread(wget.download)(
+            image, os.path.join(Config.DOWN_PATH, "imdb_thumb.jpg")
+        )
         optimize_image(img_path)
         await message.client.send_photo(
-            chat_id=message.chat.id,
-            photo=img_path,
-            caption=des_,
-            parse_mode="html"
+            chat_id=message.chat.id, photo=img_path, caption=des_, parse_mode="html"
         )
         await message.delete()
         os.remove(img_path)
@@ -94,7 +90,9 @@ async def imdb(message: Message):
 def optimize_image(path):
     _image = Image.open(path)
     if _image.size[0] > 720:
-        _image.resize((720, round(truediv(*_image.size[::-1]) * 720))).save(path, quality=95)
+        _image.resize((720, round(truediv(*_image.size[::-1]) * 720))).save(
+            path, quality=95
+        )
 
 
 def get_movie_details(soup):
@@ -107,9 +105,9 @@ def get_movie_details(soup):
     if tags:
         mov_details.append(tags)
     if mov_details and len(mov_details) > 1:
-        mov_details_text = ' | '.join(mov_details)
+        mov_details_text = " | ".join(mov_details)
     else:
-        mov_details_text = mov_details[0] if mov_details else ''
+        mov_details_text = mov_details[0] if mov_details else ""
     return mov_details_text
 
 
@@ -119,14 +117,14 @@ def get_countries_and_languages(soup):
     lg_text = ""
     if languages:
         if len(languages) > 1:
-            lg_text = ', '.join([lng["NAME"] for lng in languages])
+            lg_text = ", ".join([lng["NAME"] for lng in languages])
         else:
             lg_text = languages[0]["NAME"]
     else:
         lg_text = "Not Found!"
     if countries:
         if len(countries) > 1:
-            ct_text = ', '.join([ctn["NAME"] for ctn in countries])
+            ct_text = ", ".join([ctn["NAME"] for ctn in countries])
         else:
             ct_text = countries[0]["NAME"]
     else:
@@ -141,21 +139,21 @@ def get_credits_text(soup):
     actor = pg.get("Stars")
     if direc:
         if len(direc) > 1:
-            director = ', '.join([x["NAME"] for x in direc])
+            director = ", ".join([x["NAME"] for x in direc])
         else:
             director = direc[0]["NAME"]
     else:
         director = "Not Found!"
     if writer:
         if len(writer) > 1:
-            writers = ', '.join([x["NAME"] for x in writer])
+            writers = ", ".join([x["NAME"] for x in writer])
         else:
             writers = writer[0]["NAME"]
     else:
         writers = "Not Found!"
     if actor:
         if len(actor) > 1:
-            actors = ', '.join([x["NAME"] for x in actor])
+            actors = ", ".join([x["NAME"] for x in actor])
         else:
             actors = actor[0]["NAME"]
     else:
