af42d7b51a5aade3ec801c4dacba38b63ffece95
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2021-12-19 23:54:26 +0530

    Update __init__.py

--- paimon/plugins/alicia/__init__.py
+++ paimon/plugins/alicia/__init__.py
@@ -5,10 +5,8 @@ from pyrogram import Client
 TRIGGERS = os.environ.get("TRIGGERS", "/ !").split()
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-BOT_NAME = os.environ.get("BOT_NAME")
+BOT_NAME = "Paimon"
 DB_URL = os.environ.get("DATABASE_URL")
-ANILIST_CLIENT = os.environ.get("ANILIST_CLIENT")
-ANILIST_SECRET = os.environ.get("ANILIST_SECRET")
 ANILIST_REDIRECT_URL = os.environ.get(
     "ANILIST_REDIRECT_URL", "https://anilist.co/api/v2/oauth/pin"
 )
