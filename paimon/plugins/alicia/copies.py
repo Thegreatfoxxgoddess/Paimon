19eaa600367c7709da598a50c80e251822f8cdd0
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-01-16 23:39:57 +0530

    Update copies.py

--- paimon/plugins/alicia/copies.py
+++ paimon/plugins/alicia/copies.py
@@ -1,4 +1,4 @@
-# plugin made for paimon-X by @Kakashi_HTK(TG)/@ashwinstr(GH)
+# plugin made for paimon by @Kakashi_HTK(TG)/@ashwinstr(GH)
 # before porting please ask to Kakashi
 
 
@@ -17,10 +17,10 @@ CHANNEL = paimon.getCLogger(__name__)
 
 
 @paimon.on_cmd(
-    "copy_ch",
+    "copych",
     about={
         "header": "copy your channel content",
-        "description": "copy your channel content from one channel to another\nNOTE: it'll post in latest to older order",
+        "description": "copy your channel content from one channel to another",
         "usage": "{tr}copy_ch [from_channel_id] [to_channel_id]",
     },
 )
@@ -32,16 +32,20 @@ async def copy_channel_(message: Message):
     from_chann = input_.split()[0]
     to_chann = input_.split()[1]
     try:
-        if from_chann.isdigit():
+        try:
             from_chann = int(from_chann)
+        except:
+            pass
         from_ = await paimon.get_chat(from_chann)
-    except BaseException:
+    except BaseException as e:
         return await message.edit(
-            f"`Given from_channel '{from_chann}' is invalid...`", del_in=5
+            f"`Given from_channel '{from_chann}' is invalid...`\nERROR: {str(e)}", del_in=5
         )
     try:
-        if to_chann.isdigit():
+        try:
             to_chann = int(to_chann)
+        except:
+            pass
         to_ = await paimon.get_chat(to_chann)
     except BaseException:
         return await message.edit(
@@ -62,31 +66,22 @@ async def copy_channel_(message: Message):
             f"Need admin rights to copy posts to {to_.title}...", del_in=5
         )
     total = 0
-    del_list = []
+    list_ = []
     await message.edit(
         f"`Copying posts from `<b>{from_.title}</b>` to `<b>{to_.title}</b>..."
     )
     async for post in paimon.search_messages(from_.id):
-        total += 1
-        try:
-            # first posting, which'll be in reverse order of original posts
-            first_post = await paimon.copy_messages(to_.id, from_.id, post.message_id)
-            del_list.append(first_post.message_id)
-        except FloodWait as e:
-            await asyncio.sleep(e.x + 3)
-    await message.edit(
-        f"`Posting the {total} posts again to correct the their order...`"
-    )
-    for post_again in del_list:
-        try:
-            # second posting to correct the order
-            await paimon.copy_messages(message.chat.id, message.chat.id, post_again)
-        except FloodWait as e:
-            await asyncio.sleep(e.x + 3)
+        list_.append(post.message_id)
+    list_.reverse()
     try:
-        await paimon.delete_messages(del_list)
+        for one_msg in list_:
+            await paimon.copy_message(to_.id, from_.id, one_msg)
+            total += 1
     except FloodWait as e:
         await asyncio.sleep(e.x + 3)
+    except Exception as e:
+        await CHANNEL.log(f"ERROR: {str(e)}")
+        return await message.edit("`Something went wrong, see log channel for error...`")
     out_ = f"`Forwarded `<b>{total}</b>` from `<b>{from_.title}</b>` to `<b>{to_.title}</b>`.`"
     await message.edit(out_)
     await CHANNEL.log(out_)
