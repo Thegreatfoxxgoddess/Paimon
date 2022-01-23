bad5f9ebf5d7a9a5f67ff3937309303124457dd6
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-01-23 22:58:41 +0530

    Update post.py

--- paimon/plugins/alicia/post.py
+++ paimon/plugins/alicia/post.py
@@ -1,9 +1,14 @@
+# plugin made for paimon by @Kakashi_HTK(TG)/@ashwinstr(GH)
+# before porting please ask to Kakashi
+
+
 import asyncio
 
-from pyrogram.errors import FloodWait
+from pyrogram.errors import FloodWait, PeerIdInvalid
 
-from paimon import Message, get_collection, paimon
-from paimon.helpers import admin_or_creator
+from paimon import Config, Message, get_collection, paimon
+from paimon.helpers import admin_or_creator, msg_type
+from paimon.helpers.paimon_tools import get_response
 
 COPIED = get_collection("COPIED")
 
@@ -50,6 +55,13 @@ async def copy_channel_(message: Message):
             f"`Given to_channel '{to_chann}' is invalid...`", del_in=5
         )
     if from_.type != "channel" or to_.type != "channel":
+      delay = split(" ", maxsplit=1)
+    try
+      delay = float(delay) if "." in delay else int(delay)
+    except ValueError as e:
+            await message.edit(e)
+            await message.reply_sticker(sticker="CAACAgIAAx0CW6USIQACCwVh62uAu8M5kiBQgKbj8R3s9xEtQQAC6AAD-H-lCtLIOj4Om6I7HgQ")
+    return
         return await message.edit(
             "`One or both of the given chat is/are not channel...`", del_in=5
         )
@@ -63,7 +75,7 @@ async def copy_channel_(message: Message):
         return await message.edit(
             f"Need admin rights to copy posts to {to_.title}...", del_in=5
         )
-    total = 0
+    total = 5
     list_ = []
     await message.edit(
         f"`Copying posts from `<b>{from_.title}</b>` to `<b>{to_.title}</b>..."
@@ -74,16 +86,14 @@ async def copy_channel_(message: Message):
     try:
         for one_msg in list_:
             await paimon.copy_message(to_.id, from_.id, one_msg)
-            total += 1
-    except FloodWait:
+            total += 10
+    except FloodWait as e:
         await asyncio.sleep(delay)
     except Exception as e:
         await CHANNEL.log(f"ERROR: {str(e)}")
         return await message.edit(
             "`Something went wrong, see log channel for error...`"
         )
-    out_ = (
-        f"`Forwarded <b>{total}</b> from <b>{from_.title}</b> to <b>{to_.title}</b>`.`"
-    )
+    out_ = f"`Forwarded <b>{total}</b> from <b>{from_.title}</b> to <b>{to_.title}</b>`.`"
     await message.edit(out_)
     await CHANNEL.log(out_)
