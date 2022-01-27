1625b44920924761438db737b7267b39660d281e
thegreatfoxxgoddess <thegreatfoxxgoddess@yandex.com>
2022-01-27 14:42:59 +0000

    code check

--- paimon/plugins/utils/json.py
+++ paimon/plugins/utils/json.py
@@ -1,10 +1,15 @@
+from paimon import Message, paimon
 
-from paimon import paimon, Message
 
-
-@paimon.on_cmd("json", about={
-    'header': "message object to json",
-    'usage': "reply {tr}json to any message"})
+@paimon.on_cmd(
+    "json",
+    about={
+        "header": "message object to json",
+        "usage": "reply {tr}json to any message",
+    },
+)
 async def jsonify(message: Message):
     msg = str(message.reply_to_message) if message.reply_to_message else str(message)
-    await message.edit_or_send_as_file(text=msg, filename="json.txt", caption="Too Large")
+    await message.edit_or_send_as_file(
+        text=msg, filename="json.txt", caption="Too Large"
+    )
