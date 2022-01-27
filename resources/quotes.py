4e15af3db128bcfed78ac588f74c23c4f540749c
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-01-27 23:07:27 +0530

    Update quotes.py

--- resources/quotes.py
+++ resources/quotes.py
@@ -1,7 +1,6 @@
 #playlist for now playing
 
 MUSIC_LIST = [
-     
     "Now Playing : you're mine - Kina | t.me/my_thingsuwu",
     "Now Playing : Good 4 you - Olivia Rodriguez | t.me/my_thingsuwu"]
 
