4d6828d801ba9adf72663df67597067b076e9dd6
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-01-28 22:54:16 +0530

    Update post.py

--- paimon/plugins/alicia/post.py
+++ paimon/plugins/alicia/post.py
@@ -83,7 +83,6 @@ async def copy_channel_(message: Message):
     async for post in paimon.search_messages(from_.id):
         list_.append(post.message_id)
     list_.reverse()
-    delay = split(" ", maxsplit=1)
     try:
         for one_msg in list_:
             await paimon.copy_message(to_.id, from_.id, one_msg)
