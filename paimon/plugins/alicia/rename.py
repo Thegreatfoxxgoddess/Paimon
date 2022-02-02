567e2908084e985370857aa7354b1b93aa984c87
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-02-02 22:39:00 +0530

    Update rename.py

--- paimon/plugins/alicia/rename.py
+++ paimon/plugins/alicia/rename.py
@@ -3,6 +3,18 @@ from paimon import Config, Message, paimon
 from paimon.plugins.misc.download import tg_download, url_download
 from paimon.utils import humanbytes, progress, take_screen_shot
 from paimon.utils.exceptions import ProcessCanceled
+import io
+import os
+import re
+import time
+from datetime import datetime
+from pathlib import Path
+from paimon import Config, Message, paimon
+from paimon.plugins.misc.download import tg_download, url_download
+from paimon.utils import humanbytes, progress, take_screen_shot
+from paimon.utils.exceptions import ProcessCanceled
+
+
 
 @paimon.on_cmd(
     "r",
