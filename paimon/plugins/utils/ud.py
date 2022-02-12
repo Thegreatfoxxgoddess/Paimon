b1da4dbf42f9ea2a8ba10d5615d7b1d80c03964c
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-02-13 00:02:30 +0530

    Update ud.py

--- paimon/plugins/utils/ud.py
+++ paimon/plugins/utils/ud.py
@@ -1,28 +1,23 @@
-# Copyright (C) 2020-2022 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
+# Copyright (C) 2020-2022 by paimonTeam@Github, < https://github.com/paimonTeam >.
 #
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
+# This file is part of < https://github.com/paimonTeam/paimon > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/UsergeTeam/Userge/blob/master/LICENSE >
+# Please see < https://github.com/paimonTeam/paimon/blob/master/LICENSE >
 #
-
 # All rights reserved.
 
 from urllib.error import HTTPError
 
 import urbandict
 
-from paimon import Message, paimon
+from paimon import paimon, Message
 
 
-@paimon.on_cmd(
-    "ud",
-    about={
-        "header": "Searches Urban Dictionary for the query",
-        "flags": {"-l": "limit : defaults to 1"},
-        "usage": "{tr}ud [flag] [query]",
-        "examples": ["{tr}ud paimon", "{tr}ud -l3 paimon"],
-    },
-)
+@paimon.on_cmd("ud", about={
+    'header': "Searches Urban Dictionary for the query",
+    'flags': {'-l': "limit : defaults to 1"},
+    'usage': "{tr}ud [flag] [query]",
+    'examples': ["{tr}ud paimon", "{tr}ud -l3 paimon"]})
 async def urban_dict(message: Message):
     await message.edit("Processing...")
     query = message.filtered_input_str
@@ -34,13 +29,11 @@ async def urban_dict(message: Message):
     except HTTPError:
         await message.edit(f"Sorry, couldn't find any results for: `{query}`", del_in=5)
         return
-    output = ""
-    limit = int(message.flags.get("-l", 1))
+    output = ''
+    limit = int(message.flags.get('-l', 1))
     for i, mean_ in enumerate(mean, start=1):
-        output += (
-            f"{i}. **{mean_['def']}**\n"
-            + f"  Examples:\n  * `{mean_['example'] or 'not found'}`\n\n"
-        )
+        output += f"{i}. **{mean_['def']}**\n" + \
+            f"  Examples:\n  * `{mean_['example'] or 'not found'}`\n\n"
         if limit <= i:
             break
     if not output:
