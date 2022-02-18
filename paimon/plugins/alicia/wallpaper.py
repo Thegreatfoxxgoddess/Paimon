214846ff5673387eb5d40ef5beb27a4bda5dffd9
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-02-19 00:44:09 +0530

    Update wallpaper.py

--- paimon/plugins/alicia/wallpaper.py
+++ paimon/plugins/alicia/wallpaper.py
@@ -8,16 +8,13 @@ from paimon import Message, paimon
     "wallpaper",
     about={
         "header": "Search Wallpaper",
-        "flags": {
-            "-l": "Limit of Wallpapers",
-            "-doc": "Send as Documents (Recommended)",
         },
         "description": "Search and Download Hd Wallpaper from Unsplash and upload to Telegram",
         "usage": "{tr}wall [Query]",
         "examples": "{tr}wall luffy",
     },
 )
-async def wall_(msg: Message):
+async def wall_(message: Message):
     r = requests.get("https://nekos.life/api/v2/img/wallpaper")
     g = r.json().get("url")
     await paimon.send_document(
