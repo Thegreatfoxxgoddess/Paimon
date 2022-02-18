6a3f56865721db215c382672691f94dbe1bc6cbd
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-02-19 01:11:15 +0530

    Update wallpaper.py

--- paimon/plugins/alicia/wallpaper.py
+++ paimon/plugins/alicia/wallpaper.py
@@ -14,7 +14,7 @@ from paimon import Message, paimon
     },
 )
 async def wall_(message: Message):
-    await message.edit("`searching ...`")
+    await message.edit("`searching ...`", del_in=3)
     r = requests.get("https://nekos.life/api/v2/img/wallpaper")
     g = r.json().get("url")
     await message.client.send_document(
