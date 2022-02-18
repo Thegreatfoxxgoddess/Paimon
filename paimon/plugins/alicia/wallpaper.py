25a4cf6b496b06c6dec561b9ad1d204a991b770e
avasantiago <fridaellora@yandex.com>
2022-02-18 18:49:23 +0000

    análise

--- paimon/plugins/alicia/wallpaper.py
+++ paimon/plugins/alicia/wallpaper.py
@@ -1,24 +1,26 @@
-import re
-
-import httpx
 import requests
-from pyrogram import Client, filters, types
 from pyrogram.types import Message
 
 from paimon import paimon
 
 
-@paimon.on_cmd("wallpaper", about={
-    'header': "Search Wallpaper",
-    'flags': {
-        '-l': "Limit of Wallpapers",
-        '-doc': "Send as Documents (Recommended)"
+@paimon.on_cmd(
+    "wallpaper",
+    about={
+        "header": "Search Wallpaper",
+        "flags": {
+            "-l": "Limit of Wallpapers",
+            "-doc": "Send as Documents (Recommended)",
+        },
+        "description": "Search and Download Hd Wallpaper from Unsplash and upload to Telegram",
+        "usage": "{tr}wall [Query]",
+        "examples": "{tr}wall luffy",
     },
-    'description': 'Search and Download Hd Wallpaper from Unsplash and upload to Telegram',
-    'usage': "{tr}wall [Query]",
-    'examples': "{tr}wall luffy"})
+)
 async def wall_(msg: Message):
     r = requests.get("https://nekos.life/api/v2/img/wallpaper")
     g = r.json().get("url")
-    await paimon.send_document(message.chat.id, g,)
-
+    await paimon.send_document(
+        message.chat.id,
+        g,
+    )
