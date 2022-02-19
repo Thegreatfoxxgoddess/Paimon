d0ae1a0a211c062e5cac2b75c6876ab4c5002ca7
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-02-19 23:49:20 +0530

    Update wallpaper.py

--- paimon/plugins/alicia/wallpaper.py
+++ paimon/plugins/alicia/wallpaper.py
@@ -1,8 +1,8 @@
 import requests
+import os
 from pyrogram.types import Message
-
-from paimon import Message, paimon
-
+from paimon import paimon, Message
+WALL_H_API = os.environ.get("WALL_H_API")
 
 @paimon.on_cmd(
     "wallpaper",
@@ -21,3 +21,44 @@ async def wall_(message: Message):
         message.chat.id,
         g,
     )
+
+
+
+
+
+
+
+
+
+@paimon.on_cmd(
+    "walls",
+    about={
+        "header": "fetch walls from WallHeaven",
+        "flags": {
+            "-n": "nsfw",
+        },
+        "usage": "{tr}walls",
+    },
+)
+async def wall_heaven(message: Message):
+    "fetch walls from WallHeaven"
+    if WALL_H_API:
+        api_ = WALL_H_API
+        api_found = True
+    else:
+        api_found = False
+    query_ = message.filtered_input_str
+    if not query_:
+        query_ = "wallpaper"
+    link_ = "https://wallhaven.cc/api/v1/search"
+    if api_found:
+        link_ += f"?apikey={api_}"
+    pure = '001' if "-n" in message.flags else '110'
+    param = {'q': query_, 'sorting': 'random', 'purity': pure}
+    req = requests.get(link_, params=param)
+    r = req.json().get('data')
+#    await message.reply_or_send_as_file(r)
+    try:
+        await paimon.send_photo(message.chat.id, r[0]['url'])
+    except:
+        await message.edit(r[0]['url'])
