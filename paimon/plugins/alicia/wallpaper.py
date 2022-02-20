cbc8ce64c5afb780c1774eb29294ed515f4f7ad1
avasantiago <fridaellora@yandex.com>
2022-02-20 11:53:00 +0000

    análise

--- paimon/plugins/alicia/wallpaper.py
+++ paimon/plugins/alicia/wallpaper.py
@@ -2,8 +2,7 @@ import os
 
 import requests
 from pyrogram.types import Message
-from pyrogram.errors import MediaEmpty, WebpageCurlFailed
-from wget import download
+
 from paimon import Message, paimon
 
 WALL_H_API = os.environ.get("WALL_H_API")
@@ -50,15 +49,15 @@ async def wall_heaven(message: Message):
     link_ = "https://wallhaven.cc/api/v1/search"
     if api_found:
         link_ += f"?apikey={api_}"
-    pure = '001' if "-n" in message.flags else '110'
-    param = {'q': query_, 'sorting': 'random', 'purity': pure}
+    pure = "001" if "-n" in message.flags else "110"
+    param = {"q": query_, "sorting": "random", "purity": pure}
     req = requests.get(link_, params=param)
-    r = req.json().get('data')
-#    await message.reply_or_send_as_file(r)
+    r = req.json().get("data")
+    #    await message.reply_or_send_as_file(r)
     try:
-        await paimon.send_photo(message.chat.id, r[0]['url'])
-    except:
-        await message.edit(r[0]['url'])
+        await paimon.send_photo(message.chat.id, r[0]["url"])
+    except BaseException:
+        await message.edit(r[0]["url"])
 
 
 async def send_walls(message: Message, link: str):
