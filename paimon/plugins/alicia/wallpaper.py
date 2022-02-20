14b9dbd60b4c4fcdd0cf2f3f8f16f1d668709f50
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-02-20 17:21:33 +0530

    Update wallpaper.py

--- paimon/plugins/alicia/wallpaper.py
+++ paimon/plugins/alicia/wallpaper.py
@@ -30,21 +30,35 @@ async def wall_(message: Message):
 @paimon.on_cmd(
     "wallpaper",
     about={
-        "header": "Search Wallpaper",
-        "description": "Search and Download Wallpapers from Nekos-life and upload to Telegram",
-        "usage": "{tr}wall [Query]",
-        "examples": "{tr}wall paimon",
+        "header": "fetch walls from WallHeaven",
+        "flags": {
+            "-n": "nsfw",
+        },
+        "usage": "{tr}walls",
     },
 )
-async def wall_(message: Message):
-    await message.edit("`searching ...`", del_in=3)
-    r = requests.get("https://wallhaven.cc/api/v1/collections")
-    r.json().get("url")
-    await send_walls(message, link)
-except (MediaEmpty, WebpageCurlFailed):
-    link = download(link)
-    await send_walls(message, link)
-    os.remove(link)
+async def wall_heaven(message: Message):
+    "fetch walls from WallHeaven"
+    if WALL_H_API:
+        api_ = WALL_H_API
+        api_found = True
+    else:
+        api_found = False
+    query_ = message.filtered_input_str
+    if not query_:
+        query_ = "wallpaper"
+    link_ = "https://wallhaven.cc/api/v1/search"
+    if api_found:
+        link_ += f"?apikey={api_}"
+    pure = '001' if "-n" in message.flags else '110'
+    param = {'q': query_, 'sorting': 'random', 'purity': pure}
+    req = requests.get(link_, params=param)
+    r = req.json().get('data')
+#    await message.reply_or_send_as_file(r)
+    try:
+        await paimon.send_photo(message.chat.id, r[0]['url'])
+    except:
+        await message.edit(r[0]['url'])
 
 
 async def send_walls(message: Message, link: str):
