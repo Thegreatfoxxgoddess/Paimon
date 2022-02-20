bf52455db63c1e79a02ab831924d737f8c9e7673
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-02-20 16:50:41 +0530

    Update wallpaper.py

--- paimon/plugins/alicia/wallpaper.py
+++ paimon/plugins/alicia/wallpaper.py
@@ -26,35 +26,42 @@ async def wall_(message: Message):
     )
 
 
+
+
 @paimon.on_cmd(
     "wallpaper",
     about={
-        "header": "fetch walls from WallHeaven",
-        "flags": {
-            "-n": "nsfw",
-        },
-        "usage": "{tr}wallpaper",
+        "header": "Search Wallpaper",
+        "description": "Search and Download Wallpapers from Nekos-life and upload to Telegram",
+        "usage": "{tr}wall [Query]",
+        "examples": "{tr}wall paimon",
     },
 )
-async def wall_heaven(message: Message):
-    "fetch walls from WallHeaven"
-    if WALL_H_API:
-        api_ = WALL_H_API
-        api_found = True
+async def wall_(message: Message):
+    await message.edit("`searching ...`", del_in=3)
+    r = requests.get("https://wallhaven.cc/api/v1/collections")
+    g = r.json().get("url")
+    link = (await client.image(NSFWImageTags[input_choice])).url
+        else:
+            await message.err(
+                "Choose a valid Input !, See Help for more info.", del_in=5
+            )
+            return
     else:
-        api_found = False
-    query_ = message.filtered_input_str
-    if not query_:
-        query_ = "wallpaper"
-    link_ = "https://wallhaven.cc/api/v1/search"
-    if api_found:
-        link_ += f"?apikey={api_}"
-    pure = "001" if "-n" in message.flags else "110"
-    param = {"q": query_, "sorting": "random", "purity": pure}
-    req = requests.get(link_, params=param)
-    r = req.json().get("data")
-    #    await message.reply_or_send_as_file(r)
+        link = (await client.random_image()).url
+
+    await message.delete()
+
     try:
-        await paimon.send_document(message.chat.id, r[0]["url"])
-    except BaseException:
-        await message.edit(r[0]["url"])
+        await send_nekos(message, link)
+    except (MediaEmpty, WebpageCurlFailed):
+        link = download(link)
+        await send_walls(message, link)
+        os.remove(link)
+
+async def send_walls(message: Message, link: str):
+        await message.client.send_photo(
+          message.chat.id,
+          g,
+    )
+         
