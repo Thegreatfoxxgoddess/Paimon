29dcd1d3eb4e9b396254057d4ff4fd1ad816364c
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-02-20 23:03:42 +0530

    Update wallpaper.py

--- paimon/plugins/alicia/wallpaper.py
+++ paimon/plugins/alicia/wallpaper.py
@@ -1,5 +1,6 @@
-import os
+# wallpaper plugin made by @eightbituwu and @kakashi_htk
 
+import os
 import requests
 from pyrogram.errors import MediaEmpty, WebpageCurlFailed
 from pyrogram.types import Message
@@ -10,31 +11,35 @@ from paimon import Message, paimon
 WALL_H_API = os.environ.get("WALL_H_API")
 
 
+
 @paimon.on_cmd(
-    "walls",
+    "wall",
     about={
         "header": "Search Wallpaper",
         "description": "Search and Download Wallpapers from Nekos-life and upload to Telegram",
         "usage": "{tr}wall [Query]",
-        "examples": "{tr}walls paimon",
+        "examples": "{tr}wall paimon",
     },
 )
 async def wall_(message: Message):
-    request = requests.get("https://nekos.life/api/v2/img/wallpaper")
-    grab = request.json().get("url")
-    await message.paimon.send_document(
+    await message.edit("`searching ...`")
+    await message.edit("`searching ...`", del_in=3)
+    r = requests.get("https://nekos.life/api/v2/img/wallpaper")
+    g = r.json().get("url")
+    await message.client.send_document(
         message.chat.id,
-        grab,
+        g,
     )
+
+
+
+WALL_H_API = os.environ.get("WALL_H_API")
 
 
 @paimon.on_cmd(
     "wallpaper",
     about={
         "header": "fetch walls from WallHeaven",
-        "flags": {
-            "-n": "nsfw",
-        },
         "usage": "{tr}walls",
     },
 )
@@ -51,22 +56,12 @@ async def wall_heaven(message: Message):
     link_ = "https://wallhaven.cc/api/v1/search"
     if api_found:
         link_ += f"?apikey={api_}"
-    pure = "001" if "-n" in message.flags else "110"
-    param = {"q": query_, "sorting": "random", "purity": pure}
+    pure = '001' if "-n" in message.flags else '110'
+    param = {'q': query_, 'sorting': 'random', 'purity': pure}
     req = requests.get(link_, params=param)
-    r = req.json().get("data")
-    #    await message.reply_or_send_as_file(r)
+    r = req.json().get('data')
+#    await message.reply_or_send_as_file(r)
     try:
-        await paimon.send_wallpaper(message, r[0]["url"])
-    except (MediaEmpty, WebpageCurlFailed):
-        url = download(url)
-        await send_wallpaper(message, url)
-        os.remove(url)
-
-
-async def send_wallpaper(message: Message, link: str):
-    reply = message.reply_to_message
-    reply_id = reply.message_id if reply else None
-    await message.client.send_photo(
-        chat_id=message.chat.id, photo=url, reply_to_message_id=reply_id
-    )
+        await paimon.send_photo(message.chat.id, r[0]['url'])
+    except:
+        await message.edit(r[0]['url'])
