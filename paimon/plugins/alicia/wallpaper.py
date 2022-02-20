e06ae6297f6fd3f3cd531b478c1c6631738cedaa
avasantiago <fridaellora@yandex.com>
2022-02-20 17:35:38 +0000

    análise

--- paimon/plugins/alicia/wallpaper.py
+++ paimon/plugins/alicia/wallpaper.py
@@ -1,15 +1,13 @@
 # wallpaper plugin made by @eightbituwu and @kakashi_htk
 
 import os
+
 import requests
-from pyrogram.errors import MediaEmpty, WebpageCurlFailed
 from pyrogram.types import Message
-from wget import download
 
 from paimon import Message, paimon
 
 WALL_H_API = os.environ.get("WALL_H_API")
-
 
 
 @paimon.on_cmd(
@@ -32,8 +30,6 @@ async def wall_(message: Message):
     )
 
 
-
-
 @paimon.on_cmd(
     "wallpaper",
     about={
@@ -54,12 +50,12 @@ async def wall_heaven(message: Message):
     link_ = "https://wallhaven.cc/api/v1/search"
     if api_found:
         link_ += f"?apikey={api_}"
-    pure = '001' if "-n" in message.flags else '110'
-    param = {'q': query_, 'sorting': 'random', 'purity': pure}
+    pure = "001" if "-n" in message.flags else "110"
+    param = {"q": query_, "sorting": "random", "purity": pure}
     req = requests.get(link_, params=param)
-    r = req.json().get('data')
-#    await message.reply_or_send_as_file(r)
+    r = req.json().get("data")
+    #    await message.reply_or_send_as_file(r)
     try:
-        await paimon.send_photo(message.chat.id, r[0]['url'])
-    except:
-        await message.edit(r[0]['url'])
+        await paimon.send_photo(message.chat.id, r[0]["url"])
+    except BaseException:
+        await message.edit(r[0]["url"])
