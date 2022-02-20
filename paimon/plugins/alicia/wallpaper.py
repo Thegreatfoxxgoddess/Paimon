970c0d8b975bdaf005289cc94d4b3ebdaa294db6
avasantiago <fridaellora@yandex.com>
2022-02-20 15:48:45 +0000

    análise

--- paimon/plugins/alicia/wallpaper.py
+++ paimon/plugins/alicia/wallpaper.py
@@ -1,12 +1,11 @@
 import os
-import asyncio
-import random
+
 import requests
 from pyrogram.errors import MediaEmpty, WebpageCurlFailed
 from pyrogram.types import Message
 from wget import download
 
-from paimon import Message,Config, paimon
+from paimon import Message, paimon
 
 WALL_H_API = os.environ.get("WALL_H_API")
 
