cac8b3dba9bbfad20c5ed5ddc6ccd509b61d4bd7
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-03-08 20:26:16 +0530

    Update kang.py

--- paimon/plugins/fun/kang.py
+++ paimon/plugins/fun/kang.py
@@ -88,12 +88,12 @@ async def kang_(message: Message):
     user = await paimon.get_me()
     replied = message.reply_to_message
     if Config.LOG_KANG:
-        await message.edit("`me stealing this...`", del_in=5)
+        await message.edit("`me stealing this...`", del_in=6)
         kang_msg = await paimon.send_message(
             Config.LOG_CHANNEL_ID, "`theft in progress...`"
         )
     else:
-        kang_msg = await message.edit("`me stealing this...`", del_in=5)
+        kang_msg = await message.edit("`me stealing this...`")
     media_ = None
     emoji_ = None
     is_anim = False
