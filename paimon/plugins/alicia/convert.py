28f7b3f07fcd40378c661ea2b9d2f7673015355f
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-04-20 00:54:01 +0530

    Update convert.py

--- paimon/plugins/alicia/convert.py
+++ paimon/plugins/alicia/convert.py
@@ -43,6 +43,32 @@ async def convert_(message: Message):
         await message.edit("Please read `.help r`", del_in=5)
 
 
+
+
+@paimon.on_cmd(
+    "re",
+    about={
+        "header": "Rename telegram files",
+        "flags": {"-d": "upload as document", "-wt": "without thumb"},
+        "usage": "{tr}rename [flags] [new_name_with_extension] : reply to telegram media",
+        "examples": "{tr}rename -d test.mp4",
+    },
+    del_pre=True,
+    check_downpath=True,
+)
+async def rename_(message: Message):
+    """rename telegram files"""
+    if not message.filtered_input_str:
+        await message.err("new name not found!")
+        return
+    await message.edit("`Trying to Rename ...`")
+    if message.reply_to_message and message.reply_to_message.media:
+        await _handle_message(message)
+    else:
+        await message.edit("Please read `.help rename`", del_in=5)
+
+
+
 async def _handle_message(message: Message) -> None:
     try:
         dl_loc, _ = await tg_download(message, message.reply_to_message)
