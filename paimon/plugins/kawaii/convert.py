f0642ee4c1c586b33d750ffbe86f9efeae2dbcce
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-04-24 10:23:26 +0530

    Update convert.py

--- paimon/plugins/kawaii/convert.py
+++ paimon/plugins/kawaii/convert.py
@@ -11,26 +11,26 @@ from paimon import Message, paimon
     "png",
     about={
         "header": "Convert telegram files",
-        "usage": "reply {tr}png em alguma foto/sticker",
+        "usage": "reply {tr}png on some photo/sticker",
     },
 )
 async def convert_(message: Message):
     """convert telegram files"""
     replied = message.reply_to_message
-    await message.edit("`Processando...`")
+    await message.edit("`processing...`")
     if not replied:
-        return await message.err("`Responda a alguma mídia.`")
+        return await message.err("`Reply to some media.`")
     media = await message.reply_to_message.download()
     if not media.endswith((".jpg", ".png", ".bmp", ".tif", ".webp")):
         os.remove(media)
         return await message.err("`Formato não suportado`")
     try:
-        await message.edit("`Convertendo...`")
+        await message.edit("`converting...`")
         img = Image.open(media).convert("RGB")
         img.save("converted.png", "png")
         await message.delete()
-        msg = "__Made by [paimon](https://t.me/fnixsup)__"
-        await paimon.send_document(
+        msg = "__Made by [paimon](https://t.me/candys_bot)__"
+        await client.send_document(
             chat_id=message.chat.id, document="converted.png", caption=msg
         )
     except Exception as e:
