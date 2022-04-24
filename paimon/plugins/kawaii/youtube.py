c611697c8010bd0b1f4ccfaa254629c52d7194a3
avasantiago <fridaellora@yandex.com>
2022-04-24 03:21:51 +0000

    análise

--- paimon/plugins/kawaii/youtube.py
+++ paimon/plugins/kawaii/youtube.py
@@ -4,31 +4,34 @@
 
 from __future__ import unicode_literals
 
-import os
 import glob
 import json
-
+import os
 from pathlib import Path
-from yt_dlp import YoutubeDL
-from youtubesearchpython import SearchVideos
 
-from paimon import paimon, Config, Message
+from youtubesearchpython import SearchVideos
+from yt_dlp import YoutubeDL
+
+from paimon import Config, Message, paimon
+
 from ..bot.utube_inline import BASE_YT_URL, get_yt_video_id
 
-
 LOGGER = paimon.getLogger(__name__)
+
 
 @paimon.on_cmd(
     "song",
     about={
         "header": "Music Downloader",
         "description": "Download songs using yt_dlp",
-        'options': {'-f': 'to download in flac format'},
-        'examples': ['{tr}song link',
-                     '{tr}song name of song',
-                     '{tr}song -f name of song']
-        }
-    )
+        "options": {"-f": "to download in flac format"},
+        "examples": [
+            "{tr}song link",
+            "{tr}song name of song",
+            "{tr}song -f name of song",
+        ],
+    },
+)
 async def song_(message: Message):
     query = message.input_str
     if not query:
@@ -40,15 +43,15 @@ async def song_(message: Message):
         "logger": LOGGER,
         "writethumbnail": True,
         "prefer_ffmpeg": True,
-        'format': 'bestaudio/best',
+        "format": "bestaudio/best",
         "geo_bypass": True,
         "nocheckcertificate": True,
         "postprocessors": [
-                {
-                     'key': 'FFmpegExtractAudio',
-                     'preferredcodec': "mp3",
-                     'preferredquality': '320',
-                 },
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "320",
+            },
             {"key": "EmbedThumbnail"},
             {"key": "FFmpegMetadata"},
         ],
@@ -56,8 +59,8 @@ async def song_(message: Message):
     }
     filename_, capt_, duration_ = extract_inf(link, aud_opts)
     if filename_ == 0:
-        _fpath = ''
-        for _path in glob.glob(os.path.join(Config.DOWN_PATH, '*')):
+        _fpath = ""
+        for _path in glob.glob(os.path.join(Config.DOWN_PATH, "*")):
             if not _path.lower().endswith((".jpg", ".png", ".webp")):
                 _fpath = _path
         if not _fpath:
@@ -74,10 +77,12 @@ async def song_(message: Message):
     about={
         "header": "Video Downloader",
         "description": "Download videos using yt_dlp",
-        'examples': ['{tr}video link',
-                     '{tr}video video name',]
-        }
-    )
+        "examples": [
+            "{tr}video link",
+            "{tr}video video name",
+        ],
+    },
+)
 async def vid_(message: Message):
     query = message.input_str
     if not query:
@@ -85,23 +90,19 @@ async def vid_(message: Message):
     await message.edit("`hold on ...`")
     vid_opts = {
         "outtmpl": os.path.join(Config.DOWN_PATH, "%(title)s.%(ext)s"),
-        'logger': LOGGER,
-        'writethumbnail': False,
-        'prefer_ffmpeg': True,
-        'format': 'bestvideo+bestaudio/best',
-        'postprocessors': [
-                {
-                    'key': 'FFmpegMetadata'
-                }
-            ],
+        "logger": LOGGER,
+        "writethumbnail": False,
+        "prefer_ffmpeg": True,
+        "format": "bestvideo+bestaudio/best",
+        "postprocessors": [{"key": "FFmpegMetadata"}],
         "quiet": True,
     }
     link = await get_link(query)
     await message.edit("`processing...`")
     filename_, capt_, duration_ = extract_inf(link, vid_opts)
     if filename_ == 0:
-        _fpath = ''
-        for _path in glob.glob(os.path.join(Config.DOWN_PATH, '*')):
+        _fpath = ""
+        for _path in glob.glob(os.path.join(Config.DOWN_PATH, "*")):
             if not _path.lower().endswith((".jpg", ".png", ".webp")):
                 _fpath = _path
         if not _fpath:
@@ -145,4 +146,3 @@ def extract_inf(url, _opts):
         return y_e
     else:
         return dloader, capt_, duration_
-
