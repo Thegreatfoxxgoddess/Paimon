48d4dd7a96506cb8b8ea665f1c88960da0f32990
Ryuk <88324835+Anonymousx97@users.noreply.github.com>
2022-04-26 15:28:08 +0530

    Update sys_tools.py

--- paimon/utils/sys_tools.py
+++ paimon/utils/sys_tools.py
@@ -9,6 +9,7 @@ from os import environ, getpid, kill
 from os.path import isfile, relpath
 from signal import SIGTERM
 from typing import Dict, List, Union
+from paimon import config
 
 _SECURE = [
     # critical
@@ -70,6 +71,6 @@ def secure_text(text: str) -> str:
         return ""
     for var in _SECURE:
         tvar = environ.get(var, None)
-        if tvar and tvar in text:
+        if tvar and tvar in text and Config.REVEAL_VAR == True:
             text = text.replace(tvar, "[SECURED!]")
     return text
