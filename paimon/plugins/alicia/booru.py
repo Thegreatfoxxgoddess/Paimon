8edac9b871976c436c4ed4594072631d902eb982
avasantiago <fridaellora@yandex.com>
2022-04-28 18:54:59 +0000

    análise

--- paimon/plugins/alicia/booru.py
+++ paimon/plugins/alicia/booru.py
@@ -1,17 +1,18 @@
-import requests
 import random
-from random import choice
+
+import requests
+
 from paimon import Message, paimon
 
-@paimon.on_cmd(
-  "boru",
-  about={"download images from danbooru"},
-)
 
+@paimon.on_cmd(
+    "boru",
+    about={"download images from danbooru"},
+)
 async def booru(message: Message):
-  query = message.input_str
-  resp = requests.get(f"https://danbooru.donmai.us/posts.json?tags={query}")
-  link = resp.json()
-  r = random.choice(link)
-  pic = r['large_file_url']
-  await message.reply_photo(pic)
+    query = message.input_str
+    resp = requests.get(f"https://danbooru.donmai.us/posts.json?tags={query}")
+    link = resp.json()
+    r = random.choice(link)
+    pic = r["large_file_url"]
+    await message.reply_photo(pic)
