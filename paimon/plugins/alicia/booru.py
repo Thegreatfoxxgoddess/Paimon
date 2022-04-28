9181498eba91567122c4bcc2f82439af9d1d3a4c
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-04-29 00:23:17 +0530

    Create booru.py

---
+++ paimon/plugins/alicia/booru.py
@@ -0,0 +1,17 @@
+import requests
+import random
+from random import choice
+from paimon import Message, paimon
+
+@paimon.on_cmd(
+  "boru",
+  about={"download images from danbooru"},
+)
+
+async def booru(message: Message):
+  query = message.input_str
+  resp = requests.get(f"https://danbooru.donmai.us/posts.json?tags={query}")
+  link = resp.json()
+  r = random.choice(link)
+  pic = r['large_file_url']
+  await message.reply_photo(pic)
