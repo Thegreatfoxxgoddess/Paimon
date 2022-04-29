325c9afb31484f5f7dcfd3a3518946d777c3d52c
avasantiago <fridaellora@yandex.com>
2022-04-29 21:25:03 +0000

    análise

--- paimon/plugins/alicia/booru.py
+++ paimon/plugins/alicia/booru.py
@@ -14,8 +14,8 @@ async def booru(message: Message):
     resp = requests.get(f"https://danbooru.donmai.us/posts.json?tags={query}")
     link = resp.json()
     r = random.choice(link)
-    pic = r["large_file_url"] 
+    pic = r["large_file_url"]
     if "-d" not in message.flags:
-        await message.reply_photo(pic) 
+        await message.reply_photo(pic)
     else:
         await message.reply_document(pic)
