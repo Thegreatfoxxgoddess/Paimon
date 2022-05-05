acc92d9638cbd84effe044118991920b44e76ae8
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-05-05 11:31:22 +0530

    Update print.py

--- paimon/plugins/kawaii/print.py
+++ paimon/plugins/kawaii/print.py
@@ -19,11 +19,11 @@ async def printer(message: Message):
     text = message.input_str
     if not text:
         return await message.edit("Please specify a url")
-    await message.edit("`Negoçando o print...`")
+    await message.edit("`taking screenshot...`")
     async with httpx.AsyncClient(http2=True, timeout=1000) as http:
         r = await http.get("https://amn-api.herokuapp.com/print", params=dict(q=text))
         bio = BytesIO(r.read())
     await http.aclose()
     bio.name = "screenshot.png"
     await message.delete()
-    await message.client.send_photo(chat_id=message.chat.id, photo=bio)
+    await message.client.send_document(chat_id=message.chat.id, photo=bio)
