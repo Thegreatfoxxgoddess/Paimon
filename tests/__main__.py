b7efa59e36abc9418d01b1321069e4a5c7043105
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-05-13 19:29:47 +0530

    Update __main__.py

--- tests/__main__.py
+++ tests/__main__.py
@@ -12,8 +12,8 @@ from paimon import paimon
 async def _worker() -> None:
     chat_id = int(os.environ.get("CHAT_ID") or 0)
     type_ = 'unofficial' if os.path.exists("../paimon/plugins/unofficial") else 'main'
-    await paimon.send_message(chat_id, f'`{type_} build concluida !`')
+    await paimon.send_message(chat_id, f'`{type_} build completed!`')
 
 if __name__ == "__main__":
     paimon.begin(_worker())
-    print('O teste paimon foi concluído!')
+    print('paimon test is complete!')
