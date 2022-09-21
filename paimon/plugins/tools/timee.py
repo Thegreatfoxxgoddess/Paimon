1bb18720bee9ff726531a784b4a8b78c07eb6133
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-09-21 23:46:24 +0530

    Create timee.py

---
+++ paimon/plugins/tools/timee.py
@@ -0,0 +1,110 @@
+ From [PaperPlane Userbot](https://github
+""" Userbot module for getting the date
+    and time of any cort os
+from datetime import datetime as dt
+
+from pytz import country_names as c_n
+from pytz import country_timezones as c_tz
+from pytz import timezone as tz
+from paimon import Message, get_collection, paimon
+
+COUNTRY_CITY = os.environ.get("COUNTRY_CITY", None)
+LOC_NAME = get_collection("LOC_NAME")
+
+
+async def get_tz(con):
+    """ Get time zone of the given country. """
+    if "(Uk)" in con:
+        con = con.replace("Uk", "UK")
+    if "(Us)" in con:
+        con = con.replace("Us", "US")
+    if " Of " in con:
+        con = con.replace(" Of ", " of ")
+    if "(Western)" in con:
+        con = con.replace("(Western)", "(western)")
+    if "Minor Outlying Islands" in con:
+        con = con.replace("Minor Outlying Islands", "minor outlying islands")
+    if "Nl" in con:
+        con = con.replace("Nl", "NL")
+
+    for c_code in c_n:
+        if con == c_n[c_code]:
+            return c_tz[c_code]
+    try:
+        if c_n[con]:
+            return c_tz[con]
+    except KeyError:
+        return
+
+
+@paimon.on_cmd(
+    "dt(?: |$)(.*)(?<![0-9])(?: |$)([0-9]+)?",
+    about={
+        "header": "Get Date and Time of a country",
+        "description": "Get the Date and Time of a country. If a country has multiple timezones, "
+        "it will list all of them and let you select one.",
+        "usage": "{tr}dt <country name/code> <timezone number>",
+        "examples": ["{tr}dt Russia 2"],
+        "default timezone": 'Choose from the <b><a href="https://pastebin.com/raw/0KSh9CMj">Timezones Avaliable</a></b>'
+        "\n and Set any of them in (<code>COUNTRY_CITY</code>) for your default timezone and see help `{tr}setloc` to set Display Location",
+    },
+)
+async def date_time_func(message: Message):
+    """get date and time"""
+    # For .dt command, return the date and time of
+    # 1. The country passed as an argument,
+    # 2. The default userbot country,
+    # 3. The server where the userbot runs.
+    con = message.matches[0].group(1).title()
+    tz_num = message.matches[0].group(2)
+    d_form = "%d/%m/%y - %A"
+    t_form = "%I:%M %p"
+    c_name = None
+
+    if len(con) > 4:
+        try:
+            c_name = c_n[con]
+        except KeyError:
+            c_name = con
+        timezones = await get_tz(con)
+    elif COUNTRY_CITY:
+        timezones = [COUNTRY_CITY]
+    else:
+        await message.edit(
+            f"it's  **{dt.now().strftime(t_form)}**"
+        )
+        return
+
+    if not timezones:
+        await message.err("Invaild country.", del_in=5)
+        return
+
+    if len(timezones) == 1:
+        time_zone = timezones[0]
+    elif len(timezones) > 1:
+        if tz_num:
+            tz_num = int(tz_num)
+            time_zone = timezones[tz_num - 1]
+        else:
+            return_str = f"`{c_name} has multiple timezones:`\n"
+
+            for i, item in enumerate(timezones):
+                return_str += f"`{i+1}. {item}`\n"
+
+            return_str += "\n`Choose one by typing the number "
+            return_str += "in the command.`\n"
+            return_str += f"Example: .dt {c_name} 2"
+
+            await message.edit(return_str)
+            return
+
+    dtnow = dt.now(tz(time_zone)).strftime(d_form)
+    dttime = dt.now(tz(time_zone)).strftime(t_form)
+
+    if not c_name:
+        s_o = await LOC_NAME.find_one({"_id": "LOC_NAME"})
+        c_name = s_o["name"] if s_o else ""
+
+    await message.edit(
+        f"<code>It's</code>  **{dttime}** <code>on</code> **{dtnow}** <code>in</code> {c_name} ({time_zone} timezone)."
+    )
